38c2532d17c919179735c7224fa363bc90808190
serhan kılıç <sserhankilic@gmail.com>
2024-10-19 16:32:47 +0300

    header background has some issues

--- restaurant-web/src/App.vue
+++ restaurant-web/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
+import Header from './components/header.vue'
 </script>
 
 <template>
 <header>
   <Navbar />
+  <Header />
+
 </header>
 
 </template>
---
+++ restaurant-web/src/components/header.vue
@@ -0,0 +1,8 @@
+<script setup>
+import bg from '../../public/bg.png'
+</script>
+
+<template>
+    <section class="bg">
+    </section>
+</template>
