4862287b058ac412d6e71f50a46781186b90fa09
serhan kılıç <sserhankilic@gmail.com>
2024-10-19 15:19:42 +0300

    first vue training started

---
+++ restaurant-web/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+<header>
+  <Navbar />
+</header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ restaurant-web/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+<nav>
+    <div class="logo">
+    <p>SERONERO'S</p>
+</div>
+<div class="buttons link">
+    <a href="#">Home</a>
+    <a href="#">Mobile App</a>
+    <a href="#">Recipes</a>
+    <a href="#">Blog</a>
+    <a href="#">Gallery</a>
+</div>
+<div class="download-button">
+    <a href="#" class="big-button">Download App</a>
+</div>
+</nav>
+</template>
